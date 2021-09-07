889bd91c80356c998ffbb5bd683a31caf990da85
CUONG PHAM CAO <1821050741@student.humg.edu.vn>
2021-09-07 08:01:56 +0700

    Add project files.

---
+++ WebApplication4/Controllers/DemoController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication4.Controllers
+{
+    public class DemoController : Controller
+    {
+        // GET: Demo
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Index(string hoten, string MaSV)
+        {
+            ViewBag.thongtin = hoten + "-" + MaSV;
+            return View();
+        }
+    }
+}
